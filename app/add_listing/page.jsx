617b4f2d93f7efd51e1ce50b70fe7b33747ef7be
Hasaan Ahmed <hasaanahmed.pk@gmail.com>
2023-08-25 03:37:45 +0500

    added create listing button on add listing page

--- app/add_listing/page.jsx
+++ app/add_listing/page.jsx
@@ -1,13 +1,18 @@
+import ActionButton from "../components/action-button/action_button";
 
 
 export default function AddListing() {
   return (
-    <div className="w-full p-20 overflow-auto">
-        <div className="w-full">
-        <h1 className="w-full text-center font-bold text-[3rem] text-transparent bg-gradient-to-r from-blue-600 to-fuchsia-500 bg-clip-text">
-            Showcase Your Startup To The World
-        </h1>
-        <p className="w-full text-center text-gray-400 font-medium text-xl">List your startup on the App Store to get customers signing up on day 1</p>
+    <div className="w-full px-60 py-20 overflow-auto">
+        <div className="w-full flex flex-col items-center">
+            <h1 className="text-center font-bold text-[3rem] text-transparent bg-gradient-to-r from-blue-600 to-fuchsia-500 bg-clip-text">
+                Let's Launch Your Startup Together
+            </h1>
+            <p className="text-gray-400 font-medium text-xl">List your startup on the App Store to reach customers faster 🚀</p>
+            <div className="mt-8">
+                <ActionButton text="Create Listing" />
+            </div>
+            <div className="w-[60vw] h-[40rem] shadow border border-gray-200 rounded-xl mt-12"></div>
         </div>
         
     </div>
