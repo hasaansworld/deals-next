84b304e3f75314130f7bf18fea95f10ae7869172
Hasaan Ahmed <hasaanahmed.pk@gmail.com>
2023-08-25 03:06:19 +0500

    added listing page heading

---
+++ app/add_listing/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function AddListing() {
+  return (
+    <div className="w-full p-20 overflow-auto">
+        <div className="w-full">
+        <h1 className="w-full text-center font-bold text-[3rem] text-transparent bg-gradient-to-r from-blue-600 to-fuchsia-500 bg-clip-text">
+            Showcase Your Startup To The World
+        </h1>
+        <p className="w-full text-center text-gray-400 font-medium text-xl">List your startup on the App Store to get customers signing up on day 1</p>
+        </div>
+        
+    </div>
+    
+  )
+}
