8a10711dc51e569da7fa86fffb753141f4944517
Hasaan Ahmed <hasaanahmed.pk@gmail.com>
2023-08-26 18:51:07 +0500

    added homepage link on logo

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -1,14 +1,17 @@
 import Image from 'next/image';
 import ActionButton from '../action-button/action-button';
+import Link from 'next/link';
 
 const Header = () => {
 	return (
-		<header className="fixed left-0 top-0 w-full bg-gray-300/10 py-2 z-10 backdrop-filter backdrop-blur-xl">
+		<header className="fixed left-0 top-0 z-10 w-full bg-gray-300/10 py-2 backdrop-blur-xl backdrop-filter">
 			<div className="container mx-auto flex items-center justify-between px-40">
-				<div className="flex items-center space-x-2">
-					<Image src="/favicon.ico" alt="Logo" width={192} height={192} className="mr-2 h-10 w-10" />
-					<h1 className="text-xl font-semibold text-gray-800 hover:text-gray-900">AppStore</h1>
-				</div>
+				<Link href="/">
+					<div className="flex items-center gap-1">
+						<Image src="/favicon.ico" alt="Logo" width={192} height={192} className="h-10 w-10" />
+						<h1 className="text-xl font-semibold text-gray-800 hover:text-gray-900">AppStore</h1>
+					</div>
+				</Link>
 
 				<div className="flex items-center space-x-6">
 					<button class="text-md rounded-xl px-4 py-2 font-semibold text-gray-900 hover:bg-gray-100">Log in</button>
