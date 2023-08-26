fbd442ad7915ac77bdb6abef66d237a9ea0ac721
Aqib Ilyas <aqibilyas.pk@gmail.com>
2023-08-27 03:02:50 +0500

    added login page

---
+++ app/login/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Pricing() {
+	return (
+		<div className="w-full overflow-auto py-32">
+			<div className="w-6/7 3xl:w-1/2 mx-auto flex flex-col items-center lg:w-5/6 xl:w-3/5">
+				<h1 className="bg-gradient-to-r from-blue-600 to-fuchsia-500 bg-clip-text text-center text-[3rem] font-bold leading-tight text-transparent">
+					Login
+				</h1>
+			</div>
+		</div>
+	);
+}
