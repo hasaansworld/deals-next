694bb12994948000687b6106a21324725b8a4c57
Aqib Ilyas <aqibilyas.pk@gmail.com>
2023-08-26 15:19:44 +0500

    add prettier config

--- app/components/action-button/action-button.jsx
+++ app/components/action-button/action-button.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
 export default function ActionButton({ text, onClick = null }) {
-
     return (
-        <button className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded-xl font-medium shadow-lg shadow-indigo-500/30 text-lg" onClick={onClick}>{text}</button>
-    )
+        <button
+            className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded-xl font-medium shadow-lg shadow-indigo-500/30 text-lg"
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    );
 }
