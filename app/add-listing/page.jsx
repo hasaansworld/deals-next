27ac503600d194e88b8b21d5e61c1a871cb5d727
Aqib Ilyas <aqibilyas.pk@gmail.com>
2023-08-26 15:37:07 +0500

    add eslint config

--- app/add-listing/page.jsx
+++ app/add-listing/page.jsx
@@ -1,21 +1,18 @@
-// import ActionButton from "../components/action-button/action-button";
+import ActionButton from '../components/action-button/action-button';
 
 export default function AddListing() {
-    return (
-        <div className="w-full px-60 py-20 overflow-auto">
-            <div className="w-full flex flex-col items-center">
-                <h1 className="text-center font-bold text-[3rem] text-transparent bg-gradient-to-r from-blue-600 to-fuchsia-500 bg-clip-text">
-                    Let's Launch Your Startup Together
-                </h1>
-                <p className="text-gray-400 font-medium text-xl">
-                    List your startup on the App Store to reach customers faster
-                    🚀
-                </p>
-                <div className="mt-8">
-                    <ActionButton text="Create Listing" />
-                </div>
-                <div className="w-[60vw] h-[40rem] shadow border border-gray-200 rounded-xl mt-12"></div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="w-full overflow-auto px-60 py-20">
+			<div className="flex w-full flex-col items-center">
+				<h1 className="bg-gradient-to-r from-blue-600 to-fuchsia-500 bg-clip-text text-center text-[3rem] font-bold text-transparent">
+					Let&apos;s Launch Your Startup Together
+				</h1>
+				<p className="text-xl font-medium text-gray-400">List your startup on the App Store to reach customers faster 🚀</p>
+				<div className="mt-8">
+					<ActionButton text="Create Listing" />
+				</div>
+				<div className="mt-12 h-[40rem] w-[60vw] rounded-xl border border-gray-200 shadow"></div>
+			</div>
+		</div>
+	);
 }
