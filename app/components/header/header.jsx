6b63b6f2240f69206affa2f4f03c47a4e9357201
hasaan12 <hasaanahmed.pk@gmail.com>
2023-10-12 16:56:39 +0500

    basic layout

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -13,21 +13,21 @@ const Header = () => {
 	console.log(pathName);
 
 	return (
-		<header className="fixed left-0 top-0 z-10 w-full bg-black py-2 backdrop-blur-2xl backdrop-filter">
-			<div className="container mx-auto flex items-center justify-between px-40">
-				<Link href="/">
+		<header className="fixed left-0 top-0 z-10 w-full bg-black/10 py-2 backdrop-blur-2xl backdrop-filter">
+			<div className="container mx-auto flex items-center px-40">
+				<Link href="/" className="flex-1">
 					<div className="flex items-center gap-2">
 						<Image src="/icon_white.svg" alt="Logo" width={350} height={280} className="h-4 w-5" />
 						<h1 className="text-xl font-semibold text-white">arsaa</h1>
 					</div>
 				</Link>
 
-				<div className="mx-32 flex w-96 items-center space-x-2 rounded-xl border border-neutral-800 bg-transparent px-4 py-1.5 transition-colors duration-300 focus-within:bg-neutral-900">
+				<div className="flex w-96 flex-1 items-center space-x-2 rounded-xl border border-neutral-800 bg-transparent px-4 py-1.5 transition-colors duration-300 focus-within:bg-neutral-900">
 					<MagnifyingGlassIcon className="h-5 w-5 text-neutral-600" />
 					<input className="h-full w-full bg-transparent text-white placeholder-white/30 focus:outline-none" placeholder="Search apps" />
 				</div>
 
-				<div className="flex items-center space-x-6">
+				<div className="flex flex-1 items-center space-x-6">
 					<FlatButton text="Blog" textColor="white/70" url="blog" textSize="text-base" size="medium" />
 					<FlatButton text="Log in" textColor="white/70" url="login" textSize="text-base" size="medium" />
 					<ActionButton
--- app/page.js
+++ app/page.js
@@ -1,11 +1,24 @@
 import '../styles/globals.css';
 
 export default function Page() {
+	const repeatedContent = Array(10).fill(null);
+
 	return (
 		<>
-			<main>
-				<div className="flex items-center justify-center p-10 text-3xl font-bold">Hello world!</div>
-			</main>
+			<div className="relative mt-20 flex flex-col items-center py-10">
+				<div className="fixed top-12 h-auto w-full bg-black/10 p-4 backdrop-blur-2xl backdrop-filter">
+					<div className="mx-auto flex w-[500px] items-center">
+						<p className="rounded-md bg-[#222] px-4 py-1 font-medium text-white">For You</p>
+					</div>
+				</div>
+				<div className="flex-1 overflow-y-auto">
+					{repeatedContent.map((item, index) => (
+						<div key={index} className="mt-5 flex min-h-[400px] min-w-[500px] items-center justify-center rounded-lg border border-[#222]">
+							<p className="text-white">Hello Post {index}</p>
+						</div>
+					))}
+				</div>
+			</div>
 		</>
 	);
 }
