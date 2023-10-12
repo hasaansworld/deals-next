00b5777227e6d7a43605b11076e9e7f1ea730ee4
hasaan12 <hasaanahmed.pk@gmail.com>
2023-10-12 19:12:46 +0500

    added notification and profile icon

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import ActionButton from '../action-button/action-button';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon, BellIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import FlatButton from '../action-button/flat-button';
 import { usePathname } from 'next/navigation';
@@ -13,8 +13,8 @@ const Header = () => {
 	console.log(pathName);
 
 	return (
-		<header className="fixed left-0 right-0 top-0 z-10 border-b border-[#111] bg-[#555]/10 py-2 backdrop-blur-2xl backdrop-filter">
-			<div className="container mx-auto flex items-center px-40">
+		<header className="fixed left-0 right-0 top-0 z-10 border-b border-[#161616] bg-[#555]/10 py-2 backdrop-blur-2xl backdrop-filter">
+			<div className="container mx-auto flex items-center px-60">
 				<Link href="/" className="flex-1">
 					<div className="flex items-center gap-2">
 						<Image src="/icon_white.svg" alt="Logo" width={350} height={280} className="h-4 w-5" />
@@ -24,18 +24,12 @@ const Header = () => {
 
 				<div className="flex w-96 flex-1 items-center space-x-2 rounded-xl border border-neutral-800 bg-transparent px-4 py-1.5 transition-colors duration-300 focus-within:bg-neutral-900">
 					<MagnifyingGlassIcon className="h-5 w-5 text-neutral-600" />
-					<input className="h-full w-full bg-transparent text-white placeholder-white/30 focus:outline-none" placeholder="Search apps" />
+					<input className="h-full w-full bg-transparent text-white placeholder-white/30 focus:outline-none" placeholder="Search software" />
 				</div>
 
-				<div className="flex flex-1 items-center space-x-6">
-					<FlatButton text="Blog" textColor="white/70" url="blog" textSize="text-base" size="medium" />
-					<FlatButton text="Log in" textColor="white/70" url="login" textSize="text-base" size="medium" />
-					<ActionButton
-						text={pathName === '/add-listing' ? 'Create Listing' : 'Add Listing'}
-						url={pathName === '/add-listing' ? 'create-listing' : 'add-listing'}
-						size="small"
-						textSize="text-sm"
-					/>
+				<div className="flex flex-1 items-center justify-end gap-6">
+					<BellIcon className="h-6 w-6 text-neutral-300" />
+					<Image src="/profile.jpg" alt="profile pic" width={400} height={400} className="h-9 w-9 rounded-full border-2 border-[#444]" />
 				</div>
 			</div>
 		</header>
--- app/page.js
+++ app/page.js
@@ -5,10 +5,13 @@ export default function Page() {
 
 	return (
 		<>
-			<div className="mt-12 flex flex-col items-center py-10">
+			<div className="mt-6 flex flex-col items-center py-10">
 				<div className="flex-1 overflow-y-auto">
 					{repeatedContent.map((item, index) => (
-						<div key={index} className="mt-5 flex min-h-[400px] min-w-[600px] items-center justify-center rounded-lg border border-[#1a1a1a]">
+						<div
+							key={index}
+							className="mt-5 flex min-h-[400px] min-w-[600px] items-center justify-center rounded-lg border border-[#1a1a1a] bg-[#040404]"
+						>
 							<p className="text-white">Hello Post {index}</p>
 						</div>
 					))}
