d0709560a56969e2fa134c04af577c75dad1055c
hasaan12 <hasaanahmed.pk@gmail.com>
2023-10-18 19:14:22 +0500

    keep background white

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Page() {
 
 	return (
 		<>
-			<div className="mt-6 flex flex-col items-center bg-[#f9f9f9] py-10">
+			<div className="mt-6 flex flex-col items-center py-10">
 				<div className="flex-1 overflow-y-auto">
 					<h2 className={`${brics.className} my-7 flex justify-center text-2xl font-bold text-black`}>
 						Share your startup story
