f6dc2c11a17bbf93985987c8e8f00b038d8fdfae
Hasaan Ahmed <hasaanahmed.pk@gmail.com>
2024-03-14 15:14:51 +0500

    removed console logs

--- app/(app)/new-listing/page.jsx
+++ app/(app)/new-listing/page.jsx
@@ -6,7 +6,6 @@ import ListingForm from './listing-form';
 export default async function NewListing() {
 	const user = await getUser();
 
-	console.log(user);
 	if (user && user.id) {
 		return <ListingForm user={user} />;
 	}
--- app/(app)/product/[id]/page.js
+++ app/(app)/product/[id]/page.js
@@ -4,7 +4,6 @@ import ProductListing from './product-listing';
 export default async function Product({ params }) {
 	const { data: details } = await getListingDetails(params.id);
 	const { data: random } = await getRandomListings();
-	console.log(random);
 
 	return <ProductListing listing={details} suggestions={random} />;
 }
