767c508248e914c8c5c2ac6f0d2a9b0b7eeb1a15
Hasaan Ahmed <hasaanahmed.pk@gmail.com>
2024-03-15 08:18:08 +0500

    added listing page metadata

--- app/(app)/product/[name_id]/page.js
+++ app/(app)/product/[name_id]/page.js
@@ -1,6 +1,16 @@
 import { getListingDetails, getRandomListings } from '@/lib/server';
 import ProductListing from './product-listing';
 
+export async function generateMetadata({ params }) {
+	const { data } = await getListingDetails(params.name_id);
+	return {
+		title: `Appdeals - ${data.appName}`,
+		openGraph: {
+			images: [data.image1],
+		},
+	};
+}
+
 export default async function Product({ params }) {
 	const { data: details } = await getListingDetails(params.name_id);
 	const { data: random } = await getRandomListings();
