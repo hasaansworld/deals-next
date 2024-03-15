426143ed4a7873a27252dd36c40eb3f536f17dbe
Hasaan Ahmed <hasaanahmed.pk@gmail.com>
2024-03-15 11:47:28 +0500

    improved footer layout on mobile

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -6,34 +6,37 @@ import AddSquareIcon from '../icons/add_square';
 const Footer = () => {
 	return (
 		<footer className="mt-10 w-full bg-gray-50 px-6 py-10">
-			<div className="flex flex-col items-center gap-4 md:mx-auto md:w-[700px] md:flex-row md:justify-between lg:w-[800px]">
-				<div className="hidden items-center justify-center gap-4 md:flex">
-					<img src="/logo_black.svg" alt="Appdeals logo dark" className="h-6 w-6" />
-					<p className="text-lg font-semibold text-neutral-400">Appdeals</p>
-					<p className="font-medium text-neutral-400">© 2024</p>
+			<div className="flex items-start justify-between gap-4 md:mx-auto md:w-[700px] md:flex-row md:items-center lg:w-[800px]">
+				<div>
+					<div className="flex items-center justify-center gap-2 md:gap-4">
+						<img src="/logo_black.svg" alt="Appdeals logo dark" className="h-6 w-6" />
+						<p className="text-lg font-semibold text-neutral-400">Appdeals</p>
+						<p className="font-medium text-neutral-400">© 2024</p>
+					</div>
+					<p className="mt-2 text-neutral-600 md:hidden">
+						Built by{' '}
+						<Link href="https://x.com/@techh2s" target="blank" className="text-fuchsia-500">
+							Hasaan Ahmed
+						</Link>
+					</p>
 				</div>
-				<div className="flex items-center gap-6 md:gap-20 lg:gap-32">
+				<div className="items-center gap-6 md:flex md:gap-20 lg:gap-32">
 					<Link href="mailto:support@appdeals.pro" className="flex items-center gap-2 text-neutral-600 md:min-w-min">
 						<Mail01Icon className="h-5 w-5" stroke="2" />
 						Contact Us
 					</Link>
-					<Link href="/submit" className="flex items-center gap-2 text-neutral-600">
+					<Link href="/submit" className="mt-2 flex items-center gap-2 text-neutral-600 md:mt-0">
 						<AddSquareIcon className="h-5 w-5" stroke="2" />
 						Submit
 					</Link>
 				</div>
 
-				<p className="text-neutral-600">
+				<p className="hidden text-neutral-600 md:block">
 					Built by{' '}
 					<Link href="https://x.com/@techh2s" target="blank" className="text-fuchsia-500">
 						Hasaan Ahmed
 					</Link>
 				</p>
-				<div className="flex items-center justify-center gap-4 md:hidden">
-					<img src="/logo_black.svg" alt="Appdeals logo dark" className="h-6 w-6" />
-					<p className="text-lg font-semibold text-neutral-400">Appdeals</p>
-					<p className="font-medium text-neutral-400">© 2024</p>
-				</div>
 				{/* <div className="grid grid-cols-2 items-start gap-3">
 					<Link href="/pricing">
 						<h5 className="cursor-pointer font-medium text-neutral-600 hover:underline">Pricing</h5>
