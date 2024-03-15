5d589eb151626ffe2aca5273365506d6ea03310d
Hasaan Ahmed <hasaanahmed.pk@gmail.com>
2024-03-15 07:23:51 +0500

    added approved, expired checks and used name_id in listings details urls

--- app/(app)/product/[name_id]/page.js
+++ app/(app)/product/[name_id]/page.js
@@ -2,7 +2,7 @@ import { getListingDetails, getRandomListings } from '@/lib/server';
 import ProductListing from './product-listing';
 
 export default async function Product({ params }) {
-	const { data: details } = await getListingDetails(params.id);
+	const { data: details } = await getListingDetails(params.name_id);
 	const { data: random } = await getRandomListings();
 
 	return <ProductListing listing={details} suggestions={random} />;
